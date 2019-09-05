2e5810b87a0165729c378425729b746c602ae923
Andy Ngo <andyngoszewee@gmail.com>
2019-09-05 15:21:12 +0800

    Always show error box to preserve spacing below Input

--- components/Input.js
+++ components/Input.js
@@ -12,7 +12,7 @@ function Input(props) {
           placeholder={props.placeholder}
           {...props}
         />
-        {props.message && <div class="mv1 h1">{props.message}</div>}
+        <div class="mv1 h1">{props.message}</div>
       </div>
     </div>
   );
