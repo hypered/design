b6c5770185d968f285abf028419ab9a1afdd1bf6
Andy Ngo <andyngoszewee@gmail.com>
2019-09-10 12:39:19 +0800

    Use .lh-copy for Layout

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import { Nav, Footer } from "../components";
 
 function Layout(props) {
   return (
-    <div className="mw8 center pa4">
+    <div className="mw8 center pa4 lh-copy">
       <Nav></Nav>
       <main>{props.children}</main>
       <Footer></Footer>
