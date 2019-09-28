e92d38636a7709a219d5f5a9a3cae94c4b61dc7a
Andy Ngo <andyngoszewee@gmail.com>
2019-09-28 17:51:13 +0800

    Add addon-info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure(
+  [
+    require.context("../stories", true, /\.stories\.js$/),
+    require.context("../components", true, /\.stories\.js$/),
+  ],
+  module,
+);
+
+addDecorator(
+  withInfo({
+    inline: true,
+  }),
+);
