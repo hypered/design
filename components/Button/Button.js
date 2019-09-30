655d1353de102de8fbb939b88031a490147ad4a4
Andy Ngo <andyngoszewee@gmail.com>
2019-09-30 16:18:57 +0800

    Refactor Button.js

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,57 +1,35 @@
-import React from "react";
-import "../../static/css/styles.css";
-import "../../static/css/tachyons.css";
+export const ButtonPrimary = props => (
+  <button className="f5 button-reset ph4 pv3 bg-black white ba bw1 b--black">
+    {props.children}
+  </button>
+);
 
-function ButtonPrimary(props) {
-  return (
-    <button className="f5 button-reset ph4 pv3 bg-black white ba bw1 b--black">
-      {props.children}
-    </button>
-  );
-}
+export const ButtonPrimaryDisabled = props => (
+  <button
+    className="button-reset ph4 pv3 bg-black white ba bw1 b--black o-50"
+    disabled
+  >
+    {props.children}
+  </button>
+);
 
-function ButtonPrimaryDisabled(props) {
-  return (
-    <button
-      className="button-reset ph4 pv3 bg-black white ba bw1 b--black o-50"
-      disabled
-    >
-      {props.children}
-    </button>
-  );
-}
+export const ButtonSecondary = props => (
+  <button className="button-reset ph4 pv3 bg-white black ba b--black bw1">
+    {props.children}
+  </button>
+);
 
-function ButtonSecondary(props) {
-  return (
-    <button className="button-reset ph4 pv3 bg-white black ba b--black bw1">
-      {props.children}
-    </button>
-  );
-}
+export const ButtonSecondaryDisabled = props => (
+  <button
+    className="button-reset ph4 pv3 bg-white black ba b--black bw1 o-50"
+    disabled
+  >
+    {props.children}
+  </button>
+);
 
-function ButtonSecondaryDisabled(props) {
-  return (
-    <button
-      className="button-reset ph4 pv3 bg-white black ba b--black bw1 o-50"
-      disabled
-    >
-      {props.children}
-    </button>
-  );
-}
-
-function ButtonFullWidth(props) {
-  return (
-    <button className="f5 button-reset ph4 pv3 bg-black white ba bw1 b--black w-100">
-      {props.children}
-    </button>
-  );
-}
-
-export {
-  ButtonPrimary,
-  ButtonPrimaryDisabled,
-  ButtonSecondary,
-  ButtonSecondaryDisabled,
-  ButtonFullWidth,
-};
+export const ButtonFullWidth = props => (
+  <button className="f5 button-reset ph4 pv3 bg-black white ba bw1 b--black w-100">
+    {props.children}
+  </button>
+);
