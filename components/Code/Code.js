64141bc4156026dcd672a1d3f6a4acbe5bcb8086
Andy Ngo <andyngoszewee@gmail.com>
2019-11-04 10:43:06 +0800

    Remove styled-component class from Code

--- components/Code/Code.js
+++ components/Code/Code.js
@@ -1,28 +1,16 @@
-import styled from "styled-components";
-
 const Pre = props => (
   <pre className="pre overflow-auto">
     <code>{props.children}</code>
   </pre>
 );
 
-const EditablePreStyled = styled.pre.attrs(props => ({
-  contentEditable: true,
-  spellCheck: false,
-  className: "pv3 ph3 mv0 relative pre overflow-auto",
-}))`
-  &[contenteditable="true"]:active,
-  &[contenteditable="true"]:focus {
-    background: #fff;
-    box-shadow: 0 0 0 1px #96ccff;
-    outline: none;
-  }
-`;
-
 const EditablePre = props => (
-  <EditablePreStyled>
+  <pre
+    className="pre editable-pre bg-woof pv3 ph3 mv0 overflow-auto"
+    contentEditable="true"
+  >
     <code>{props.children}</code>
-  </EditablePreStyled>
+  </pre>
 );
 
 export const CodeToolbar = props => (
