26575f72630e2f35b23ad29887e0ba069468ed01
Andy Ngo <andyngoszewee@gmail.com>
2019-11-06 22:21:54 +0800

    Add ASCII table examples

--- components/Code/Code.stories.js
+++ components/Code/Code.stories.js
@@ -11,6 +11,29 @@ export const CodeBlock = () => (
 `}</Code>
 );
 
+export const WithTable = () => (
+  <Code>{`
+// table examples from: https://ozh.github.io/ascii-tables/
+
+// example 1:
+┌──────────────────────────────────┬─────────┬────────────────────────┬────────────────┐
+│               Col1               │  Col2   │          Col3          │ Numeric Column │
+├──────────────────────────────────┼─────────┼────────────────────────┼────────────────┤
+│ Value 1                          │ Value 2 │ 123                    │           10.0 │
+│ Separate                         │ cols    │ with a tab or 4 spaces │       -2,027.1 │
+│ This is a row with only one cell │         │                        │                │
+└──────────────────────────────────┴─────────┴────────────────────────┴────────────────┘
+
+// example 2:
+
+|               Col1               |  Col2   |          Col3          | Numeric Column |
+|----------------------------------|---------|------------------------|----------------|
+| Value 1                          | Value 2 | 123                    |           10.0 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| This is a row with only one cell |         |                        |                |
+`}</Code>
+);
+
 export const Editable = () => (
   <Code editable>{`// This block of code can be edited.
 
