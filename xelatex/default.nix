6dcee7d05800d24cc1418624d3f99329367f58b1
Vo Minh Thu <thu@hypered.io>
2019-11-06 08:27:25 +0100

    Use IBM Plex in the example XeLaTeX document.

--- xelatex/default.nix
+++ xelatex/default.nix
@@ -6,7 +6,9 @@ in
       pkgs.texlive.combined.scheme-full
     ];
     example = ./example.tex;
+    fonts = ../docs/static/fonts;
   } ''
     mkdir -p $out
+    cp $fonts/ibm-plex-*.ttf .
     xelatex --jobname example --output-directory $out/ $example
   ''
