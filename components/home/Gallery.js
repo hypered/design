c79bacb4991e33276492be0b830f37393f951771
Andy Ngo <andyngoszewee@gmail.com>
2019-11-11 16:00:46 +0800

    Fix <a> cannot be a descendant of <a> warning

--- components/home/Gallery.js
+++ components/home/Gallery.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-const OverlayStyled = styled.div.attrs(props => ({
+const OverlayStyled = styled.a.attrs(props => ({
   className:
-    "absolute absolute--fill yellow bg-black-30 flex justify-center items-center",
+    "no-underline absolute absolute--fill yellow bg-black-30 flex justify-center items-center",
 }))`
   opacity: 0;
 `;
 
 const Overlay = props => (
-  <OverlayStyled>
+  <OverlayStyled href="#">
     <div className="inline-flex bg-black fw6 pa3">View Storybook &#8599;</div>
   </OverlayStyled>
 );
@@ -24,7 +24,7 @@ const AspectRatioObject = styled.div.attrs(props => ({
 `;
 
 export const Gallery = props => (
-  <a className="db black no-underline w-100 w-50-m w-50-l ph3 mb4" href="#">
+  <div className="db black no-underline w-100 w-50-m w-50-l ph3 mb4" href="#">
     <div className="bg-light-gray aspect-ratio aspect-ratio--7x5 relative z-0">
       <AspectRatioObject>
         <Overlay />
@@ -33,5 +33,5 @@ export const Gallery = props => (
     </div>
 
     <div className="f5 fw5 tc pt3">{props.title}</div>
-  </a>
+  </div>
 );
