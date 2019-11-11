0c598fe38bedbd4419d1f8e1c123521aa4b1e244
Andy Ngo <andyngoszewee@gmail.com>
2019-11-11 15:43:46 +0800

    Add Pill Buttons

--- components/Button/Button.stories.js
+++ components/Button/Button.stories.js
@@ -2,8 +2,12 @@ import React from "react";
 import {
   ButtonPrimary,
   ButtonPrimaryDisabled,
+  ButtonPrimaryPill,
+  ButtonPrimaryPillDisabled,
   ButtonSecondary,
   ButtonSecondaryDisabled,
+  ButtonSecondaryPill,
+  ButtonSecondaryPillDisabled,
   ButtonFullWidth,
 } from "../../components";
 
@@ -17,12 +21,32 @@ export const primaryDisabled = () => (
   <ButtonPrimaryDisabled>Primary Disabled</ButtonPrimaryDisabled>
 );
 
+export const primaryPill = () => (
+  <ButtonPrimaryPill>Primary Pill Button</ButtonPrimaryPill>
+);
+
+export const primaryPillDisabled = () => (
+  <ButtonPrimaryPillDisabled>
+    Primary Pill Disabled Button
+  </ButtonPrimaryPillDisabled>
+);
+
 export const secondary = () => (
   <ButtonSecondary>Secondary Button</ButtonSecondary>
 );
 
 export const secondaryDisabled = () => (
   <ButtonSecondaryDisabled>Secondary Disabled Button</ButtonSecondaryDisabled>
+);
+
+export const secondaryPill = () => (
+  <ButtonSecondaryPill>Secondary Pill Button</ButtonSecondaryPill>
+);
+
+export const secondaryPillDisabled = () => (
+  <ButtonSecondaryPillDisabled>
+    Secondary Pill Disabled Button
+  </ButtonSecondaryPillDisabled>
 );
 
 export const fullWidth = () => (
