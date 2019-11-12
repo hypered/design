ab7c5ad4a3930102071659e9282692c224cdae45
Andy Ngo <andyngoszewee@gmail.com>
2019-11-12 11:41:43 +0800

    Improve HeaderSection’s mobile UI

--- components/home/HeaderSection.js
+++ components/home/HeaderSection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import {
   Divider,
@@ -8,33 +8,36 @@ import {
 
 export const HeaderSection = props => (
   <>
-    <header className="pv5">
-      <h2 className="f4 mv0 lh-title relative top-1">Hypered</h2>
-      <h1
-        className="f-subheadline-m f-headline-l fw5 tracked-tight mv0 lh-title"
-        style={{ position: "relative", left: -6 }}
-      >
+    <header className="pv4 pv5-l">
+      <h2 className="f5 f4-m f4-l mv0 lh-title relative top-1-m top-1-l">
+        Hypered
+      </h2>
+      <h1 className="f2 f-subheadline-m f-headline-l fw6 fw5-m fw5-l tracked-tight mv0 lh-title relative">
         Design System
       </h1>
       {/* @TODO: replace copy */}
       <p className="f5 fw5 measure-wide lh-copy">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        convallis mollis nulla, molestie tempor velit consequat non. Integer
-        quam ligula, consequat eget semper in, sodales nec mauris. Sed ultrices
-        enim quis eros lobortis, semper condimentum eros sodales. Morbi iaculis
-        lectus id dui convallis feugiat.
+        convallis mollis nulla, molestie tempor velit consequat non.
       </p>
 
-      <div className="flex nl2 nr2 mt4">
-        <div className="ph2">
-          <ButtonLinkPrimaryPill href="#">
+      <div className="flex flex-wrap nl2 nr2 mt4">
+        <div className="w-100 w-auto-m w-auto-l ph2 mb3 mb0-m mb0-l">
+          <a
+            className="no-underline db tc ph4 pv3 bg-black white ba bw1 b--black br-pill"
+            href="#"
+          >
             <b>Browse Components</b>
-          </ButtonLinkPrimaryPill>
+          </a>
         </div>
-        <div className="ph2">
-          <ButtonLinkSecondaryPill href="#">
+
+        <div className="w-100 w-auto-m w-auto-l ph2">
+          <a
+            className="no-underline db tc ph4 pv3 bg-white black ba b--black bw1 br-pill"
+            href="#"
+          >
             Contribute on GitHub
-          </ButtonLinkSecondaryPill>
+          </a>
         </div>
       </div>
     </header>
