6b0a8f206f04cbac4f816b851a784235e6c0ebde
Andy Ngo <andyngoszewee@gmail.com>
2019-11-14 15:12:33 +0800

    Add NavBlock example

--- components/Layout/BlogPostLayout.js
+++ components/Layout/BlogPostLayout.js
@@ -11,6 +11,11 @@ import {
   Code,
   BlockQuote,
   Divider,
+  NavBlockWrapper,
+  SidebarTitle,
+  SidebarUL,
+  SidebarLI,
+  SidebarLink,
 } from "../../components";
 
 export const BlogPostLayout = props => (
@@ -78,6 +83,110 @@ export const BlogPostLayout = props => (
           culpa qui officia deserunt mollit anim id est laborum.
         </P>
       </article>
+
+      <aside className="mt4">
+        <NavBlockWrapper>
+          <div className="flex flex-wrap nl3 nr3">
+            <div className="w-100 w-25-m w-25-l ph3">
+              <SidebarTitle>Column 1</SidebarTitle>
+              <SidebarUL>
+                <SidebarLI>
+                  <SidebarLink href="#">Item One</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Two</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Three</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Four</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Five</SidebarLink>
+                </SidebarLI>
+              </SidebarUL>
+            </div>
+
+            <div className="w-100 w-25-m w-25-l ph3">
+              <SidebarTitle>Column 2</SidebarTitle>
+              <SidebarUL>
+                <SidebarLI>
+                  <SidebarLink href="#">Item One</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Two</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Three</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Four</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Five</SidebarLink>
+                </SidebarLI>
+              </SidebarUL>
+            </div>
+
+            <div className="w-100 w-25-m w-25-l ph3">
+              <SidebarTitle>Column 3</SidebarTitle>
+
+              <SidebarUL>
+                <SidebarLI>
+                  <SidebarLink href="#">Item One</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Two</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Three</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Four</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Five</SidebarLink>
+                </SidebarLI>
+              </SidebarUL>
+            </div>
+
+            <div className="w-100 w-25-m w-25-l ph3">
+              <SidebarTitle>Column 4 (Section 1)</SidebarTitle>
+              <SidebarUL>
+                <SidebarLI>
+                  <SidebarLink href="#">Item One</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Two</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Three</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Four</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Five</SidebarLink>
+                </SidebarLI>
+              </SidebarUL>
+
+              <SidebarTitle>Column 4 (Section 2)</SidebarTitle>
+              <SidebarUL>
+                <SidebarLI>
+                  <SidebarLink href="#">Item One</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Two</SidebarLink>
+                </SidebarLI>
+                <SidebarLI>
+                  <SidebarLink href="#">Item Three</SidebarLink>
+                </SidebarLI>
+              </SidebarUL>
+            </div>
+          </div>
+        </NavBlockWrapper>
+      </aside>
     </main>
   </Layout>
 );
