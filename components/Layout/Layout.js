428d2692a9468dfeb5e0e4e741e4d83ff327b076
Andy Ngo <andyngoszewee@gmail.com>
2019-11-14 14:02:52 +0800

    Fix Nav in Layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-import { Nav, Footer } from "../../components";
+import { Nav, NavLink, Footer } from "../../components";
 
 function Layout(props) {
   return (
     <div className="mw8 center pa4 lh-copy">
-      <Nav></Nav>
-      <main>{props.children}</main>
+      <Nav>
+        <NavLink active={true}>Item 1</NavLink>
+        <NavLink>Item 2</NavLink>
+        <NavLink>Item 3</NavLink>
+      </Nav>
+      {props.children}
       <Footer></Footer>
     </div>
   );
