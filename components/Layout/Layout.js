08a498c24438dcdb32f42ec4a18945ca95893af4
Andy Ngo <andyngoszewee@gmail.com>
2019-11-23 15:20:12 +0800

    Add flex, flex-column, justify-between, min-height-vh-100 to Layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,7 +4,7 @@ import { Nav, NavLink, Footer } from "../../components";
 
 function Layout(props) {
   return (
-    <div className="mw8 center pa4 lh-copy">
+    <div className="flex flex-column justify-between min-height-vh-100 mw8 center pa4 lh-copy">
       <Nav>
         <div>
           <NavLink href="#" active={true}>
