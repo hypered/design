05b56e42448e8125c51786152da3f1cbedbcdd4e
Vo Minh Thu <thu@hypered.io>
2019-11-23 11:44:12 +0100

    Group the navigation and main content together.
    This is done so that only the footer is pushed to the bottom (otherwise
    the main content is centered vertically).

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,16 +5,18 @@ import { Nav, NavLink, Footer } from "../../components";
 function Layout(props) {
   return (
     <div className="flex flex-column justify-between min-height-vh-100 mw8 center pa4 lh-copy">
-      <Nav>
-        <div>
-          <NavLink href="#" active={true}>
-            noteed.com
-          </NavLink>
-          <NavLink href="#">blog</NavLink>
-          <NavLink href="#">not-os</NavLink>
-        </div>
-      </Nav>
+      <div>
+        <Nav>
+          <div>
+            <NavLink href="#" active={true}>
+              noteed.com
+            </NavLink>
+            <NavLink href="#">blog</NavLink>
+            <NavLink href="#">not-os</NavLink>
+          </div>
+        </Nav>
       {props.children}
+      </div>
       <Footer></Footer>
     </div>
   );
