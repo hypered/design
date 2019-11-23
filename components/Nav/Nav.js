b9645de5cee6862f946076e00674cf7d8bd934d7
Andy Ngo <andyngoszewee@gmail.com>
2019-11-23 15:04:37 +0800

    Update Nav to use justify: space-between

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -10,7 +10,7 @@ export const NavLink = React.forwardRef(function NavLink(props, ref) {
 });
 
 export const Nav = props => (
-  <nav className="flex align-items-center lh-copy mb4 pv3">
+  <nav className="flex justify-between align-items-center lh-copy mb4 pv3">
     {props.children}
   </nav>
 );
