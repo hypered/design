006595bf820cea43fdd1b2211ef075647bcea4ca
Andy Ngo <andyngoszewee@gmail.com>
2019-12-18 12:22:49 +0800

    Add PullQuote example

--- components/BlockQuote/BlockQuote.js
+++ components/BlockQuote/BlockQuote.js
@@ -1,5 +1,16 @@
+import React from "react";
+import cx from "classnames";
+
 export const BlockQuote = props => (
-  <blockquote className="db bl bw2 pv2 ph3 ml0 mv4">
+  <blockquote className="db bl bw2 pv2 ph3 ml0 mv4 lh-copy">
     <span className="i">{props.children}</span>
   </blockquote>
 );
+
+export const PullQuote = props => {
+  return (
+    <blockquote className="pull-quote relative db pv3 ph4 f4 ml0 mv4 lh-copy">
+      <span className="i">{props.children}</span>
+    </blockquote>
+  );
+};
