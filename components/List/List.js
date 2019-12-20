f026f6c674a47eaa004849c6282c2761d9b5ee7d
Andy Ngo <andyngoszewee@gmail.com>
2019-12-20 21:28:30 +0800

    Prefix .ff-tab-num with .hy-

--- components/List/List.js
+++ components/List/List.js
@@ -1,5 +1,9 @@
-export const OL = props => <ol className="ff-tab-num mv3">{props.children}</ol>;
+export const OL = props => (
+  <ol className="hy-ff-tab-num mv3">{props.children}</ol>
+);
 
-export const UL = props => <ul className="ff-tab-num mv3">{props.children}</ul>;
+export const UL = props => (
+  <ul className="hy-ff-tab-num mv3">{props.children}</ul>
+);
 
 export const LI = props => <li className="mv1">{props.children}</li>;
