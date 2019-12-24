f6159b94664953f621912ea7b03fe1b89bc9a5da
Andy Ngo <andyngoszewee@gmail.com>
2019-12-24 23:18:19 +0800

    Style whitespace examples

--- components/Whitespace/whitespace.stories.js
+++ components/Whitespace/whitespace.stories.js
@@ -5,11 +5,34 @@ export default {
   title: "Whitespace",
 };
 
+const Box = ({ title, description }) => {
+  return (
+    <div className="bg-navy light-green flex justify-between h5">
+      <div className="flex flex-column justify-between pa4">
+        <h2 className="f2 fw8 lh-title mv1">{title}</h2>
+        <div>
+          <p className="f7 b tracked lh-copy ttu mv1 o-50">Description</p>
+          <p className="f5 lh-copy mv1">{description}</p>
+        </div>
+      </div>
+      <b
+        className="flex items-center justify-start pa3 bl b--light-green f6"
+        style={{ writingMode: "vertical-lr", textOrientation: "mixed" }}
+      >
+        Hypered Design System
+      </b>
+    </div>
+  );
+};
+
 export const AutoWidth = () => {
   return (
-    <div className="flex justify-center">
-      <div className="ba b--black h5 pa3 w-auto self-center">
-        This box takes up the size of its content and is centered.
+    <div className="flex justify-center mb4">
+      <div className="h5 w-auto self-center">
+        <Box
+          title="01. Auto Width"
+          description="This box takes up the size of its content and is centered."
+        />
       </div>
     </div>
   );
@@ -17,11 +40,12 @@ export const AutoWidth = () => {
 
 export const NegativeMargins = () => {
   return (
-    <div className="nl4 nr4">
-      <div className="ba b--black w-100 h5 pa3">
-        This box has negative margins and should extend beyond its parent's
-        container.
-      </div>
+    <div className="nl4 nr4 mb4">
+      <Box
+        title="02. Negative Margins"
+        description="This box has negative margins and should extend beyond its parent's
+        container."
+      />
     </div>
   );
 };
@@ -29,9 +53,10 @@ export const NegativeMargins = () => {
 export const FullWidth = () => {
   return (
     <div className="w-100">
-      <div className="ba b--black w-100 h5 pa3">
-        This box takes up the full width of its parent's container.
-      </div>
+      <Box
+        title="03. Full Width"
+        description="This box takes up the full width of its parent's container."
+      />
     </div>
   );
 };
@@ -39,9 +64,14 @@ export const FullWidth = () => {
 export const Examples = () => {
   return (
     <Layout>
-      <AutoWidth />
-      <NegativeMargins />
-      <FullWidth />
+      <div className="pa4">
+        <h1 className="dib bg-red white f6 mv0 pv1 ph2">Container</h1>
+        <div className="ba b--red debug-grid-16">
+          <AutoWidth />
+          <NegativeMargins />
+          <FullWidth />
+        </div>
+      </div>
     </Layout>
   );
 };
