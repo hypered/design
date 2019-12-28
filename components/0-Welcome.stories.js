88bbf152a32b591d6e427d095126663ed859bce1
Vo Minh Thu <thu@hypered.io>
2019-12-28 22:55:17 +0100

    Replace default Storybook Welcome page.
    In particular, this gives a link back to the design-system homepage.

--- components/0-Welcome.stories.js
+++ components/0-Welcome.stories.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
+import {
+  Layout,
+  UL,
+  OL,
+  LI,
+  H1,
+  H2,
+  H3,
+  H4,
+  P,
+  Code,
+  BlockQuote,
+  PullQuote,
+  Divider,
+  NavBlockWrapper,
+  SidebarTitle,
+  SidebarUL,
+  SidebarLI,
+  SidebarLink,
+} from "../components";
 
 export default {
   title: 'Hypered',
 };
 
-export const toStorybook = () => <Welcome showApp={linkTo('Button')} />;
+export const toStorybook = props => (
+  <Layout>
+    <main>
+      <article className="mw7 cf">
+        <div className="mb4">
+          <H1>Hypered design system</H1>
+        </div>
+
+        <P>
+          This is the Storybook-based component explorer for
+          the <a href="/design-system/">Hypered design system</a>.
+        </P>
+      </article>
+    </main>
+  </Layout>
+);
 
 toStorybook.story = {
   name: 'Design System',
