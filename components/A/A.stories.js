866d3ff33bed29a7b1f89618de913e6ccf7ee3de
Andy Ngo <andyngoszewee@gmail.com>
2019-12-30 21:35:45 +0800

    Add A stories

---
+++ components/A/A.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { A } from "../../components";
+
+export default {
+  title: "Anchor (A)",
+};
+
+export const Blue = () => (
+  <A color="blue" href="#">
+    This is a blue link
+  </A>
+);
+
+export const Black = () => (
+  <A color="black" href="#">
+    This is a black link
+  </A>
+);
