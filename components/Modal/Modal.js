79b9018e88d4a173afee975184739a294b60e42f
Andy Ngo <andyngoszewee@gmail.com>
2020-04-15 10:45:53 +0800

    Add examples and improve modal behaviour

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,16 +1,13 @@
 export const ModalOverlay = ({ children, ...props }) => (
-  <label className="bg-black-50 absolute absolute--fill z-1" {...props}>
+  <label className="bg-black-50 absolute absolute--fill z-0" {...props}>
     {children}
   </label>
 );
 
 export const ModalContainer = ({ htmlFor, children }) => (
-  <label className="modal items-center justify-center h-100 absolute absolute--fill">
+  <label className="modal items-center justify-center h-100 absolute absolute--fill z-1">
     <ModalOverlay htmlFor={htmlFor} />
-    <div
-      className="bg-white relative z-2 mw6 center"
-      style={{ flexShrink: 0, minWidth: "40rem" }}
-    >
+    <div className="bg-white relative z-3 mw6-m mw6-l center w-100 w-75-m w-50-l mh-75">
       {children}
     </div>
   </label>
@@ -23,9 +20,11 @@ export const ModalHeader = ({ children }) => (
 );
 
 export const ModalBody = ({ children }) => (
-  <div className="bl br b--black bw1 pa3">{children}</div>
+  <div className="bg-white modal-body bl br b--black bw1 pa3 overflow-y-scroll">
+    {children}
+  </div>
 );
 
 export const ModalFooter = ({ children }) => (
-  <div className="flex justify-between">{children}</div>
+  <div className="bg-blue flex justify-between">{children}</div>
 );
