e6d9da0b05ab013f350eab1b9bd5d836a91f53f9
Andy Ngo <andyngoszewee@gmail.com>
2020-04-24 00:32:51 +0800

    Add with column divider styles

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -7,60 +7,88 @@ export const Table = ({ children }) => (
   </div>
 );
 
-export const TH = ({ size, align, children }) => {
+export const TR = ({ isHeader, size, hideBottomBorder, children }) => {
+  let trClassNames = cx("b--black", {
+    bw1: isHeader,
+    bb: !hideBottomBorder,
+    "b--black": size === "normal" || !size,
+    "b--silver": size === "compact"
+  });
+  return (
+    <tr
+      className={trClassNames}
+      isHeader={isHeader}
+      size={size}
+      hideBottomBorder={hideBottomBorder}
+    >
+      {children}
+    </tr>
+  );
+};
+
+export const TH = ({ size, align, colDivider, children }) => {
   let thClassNames = cx(
     "tl",
-    "bb",
-    "bw1",
     "fw6",
     "nowrap",
     {
       f5: size === "normal" || !size,
-      pa2: size === "normal" || !size,
-      "b--black": size === "normal" || !size
+      pa2: size === "normal" || !size
     },
     {
       f6: size === "compact",
-      pa1: size === "compact",
-      "b--silver": size === "compact"
+      pa1: size === "compact"
     },
     {
       tl: align === "left" || !align,
       tr: align === "right",
       tc: align === "center"
+    },
+    {
+      br: colDivider
     }
   );
 
   return (
-    <th className={thClassNames} size={size} align={align}>
+    <th
+      className={thClassNames}
+      size={size}
+      align={align}
+      colDivider={colDivider}
+    >
       {children}
     </th>
   );
 };
 
-export const TD = ({ size, align, children }) => {
+export const TD = ({ size, align, colDivider, children }) => {
   let tdClassNames = cx(
-    "bb",
     "nowrap",
     {
       f5: size === "normal" || !size,
-      pa2: size === "normal" || !size,
-      "b--black": size === "normal" || size
+      pa2: size === "normal" || !size
     },
     {
       f6: size === "compact",
-      pa1: size === "compact",
-      "b--silver": size === "compact"
+      pa1: size === "compact"
     },
     {
       tl: align === "left" || !align,
       tr: align === "right",
       tc: align === "center"
+    },
+    {
+      br: colDivider
     }
   );
 
   return (
-    <td className={tdClassNames} size={size} align={align}>
+    <td
+      className={tdClassNames}
+      size={size}
+      align={align}
+      colDivider={colDivider}
+    >
       {children}
     </td>
   );
