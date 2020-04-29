0e271983511c294b1626e38c67f667dfdfacde8f
Andy Ngo <andyngoszewee@gmail.com>
2020-04-29 15:15:13 +0800

    Refactor StatusCode

--- components/StatusCode/StatusCode.js
+++ components/StatusCode/StatusCode.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { ContainerWithLabel } from "../../components";
+import { ContainerWithLabel, P, UL, LI, A } from "../../components";
 
 export const StatusCode = ({ status, statusCode, children }) => {
   return (
     <ContainerWithLabel label="Status">
-      <div className="pa5">
-        <h2 className="red f3 fw8 tracked-tight lh-title mv3 ttu">Error</h2>
+      <div className="pa6">
+        <h2 className="red f4 fw8 tracked-tight lh-title mv0 ttu">Error</h2>
         <h3
-          className="glitch f1 f-subheadline-m f-subheadline-l fw9 tracked-tight lh-title mv3"
+          className="glitch f1 f-subheadline-m f-subheadline-l fw9 tracked-tight lh-title mv0"
           data-text={`${statusCode} ${status}`}
         >
           {statusCode} {status}
@@ -15,5 +15,50 @@ export const StatusCode = ({ status, statusCode, children }) => {
         <div>{children}</div>
       </div>
     </ContainerWithLabel>
+  );
+};
+
+const Custom404Content = () => {
+  return (
+    <div>
+      <P>
+        Looks like the page you're looking for is unavailable. You can click
+        here to return to the home page, or visit any of the links below:
+      </P>
+
+      <UL>
+        <LI>
+          <A href="/">Home</A>
+        </LI>
+        <LI>
+          <A href="components/">Components</A>
+        </LI>
+        <LI>
+          <A href="storybook/">Storybook</A>
+        </LI>
+        <LI>
+          <A href="documentation/">Documentation</A>
+        </LI>
+        <LI>
+          <A href="haddock/">Haddock</A>
+        </LI>
+      </UL>
+    </div>
+  );
+};
+
+export const Custom400 = () => {
+  return (
+    <StatusCode status="Bad Gateway" statusCode="400">
+      <Custom404Content />
+    </StatusCode>
+  );
+};
+
+export const Custom404 = () => {
+  return (
+    <StatusCode status="Not Found" statusCode="404">
+      <Custom404Content />
+    </StatusCode>
   );
 };
--- components/StatusCode/StatusCode.stories.js
+++ components/StatusCode/StatusCode.stories.js
@@ -1,62 +1,30 @@
 import React from "react";
-import { StatusCode, P, UL, LI, A } from "../../components";
+import { Custom400, Custom404 } from "../../components";
 
 export default {
   title: "Status Codes"
 };
 
-const NotFoundContent = () => {
-  return (
-    <div>
-      <P>
-        Looks like the page you're looking for is unavailable. You can click
-        here to return to the home page, or visit any of the links below:
-      </P>
-      <UL>
-        <LI>
-          <A href="/">Home</A>
-        </LI>
-        <LI>
-          <A href="components/">Components</A>
-        </LI>
-        <LI>
-          <A href="storybook/">Storybook</A>
-        </LI>
-        <LI>
-          <A href="documentation/">Documentation</A>
-        </LI>
-        <LI>
-          <A href="haddock/">Haddock</A>
-        </LI>
-      </UL>
-    </div>
-  );
-};
-
 const Container = ({ children }) => {
   return (
-    <div className="flex items-center justify-center vh-100 mw7 center pa4">
+    <div className="flex items-center justify-center vh-100 mw8 center pa4">
       {children}
     </div>
   );
 };
 
-export const BadGateway = () => {
+export const Error400 = () => {
   return (
     <Container>
-      <StatusCode status="Bad Gateway" statusCode="400">
-        <NotFoundContent />
-      </StatusCode>
+      <Custom400 />
     </Container>
   );
 };
 
-export const NotFound = () => {
+export const Error404 = () => {
   return (
     <Container>
-      <StatusCode status="Not Found" statusCode="404">
-        <NotFoundContent />
-      </StatusCode>
+      <Custom404 />
     </Container>
   );
 };
