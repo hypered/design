3ae7802c22eb391f536170c1c9bcab1336a1466e
Vo Minh Thu <thu@hypered.io>
2023-01-23 22:30:12 +0100

    Move some code to a new module.

--- Hypered/Design/Run.hs
+++ Hypered/Design/Run.hs
@@ -8,13 +8,13 @@ import           Data.List (nub, tail)
 import qualified Data.Text                     as T
 import qualified Hypered.Design.Command        as Command
 import qualified Hypered.Design.Server         as Server
-import           Hypered.Design.Stories (stories)
+import           Hypered.Design.Stories         ( stories )
+import           Hypered.Design.Templates       ( generateTemplates )
 import Hypered.Html
-  ( Font(IbmPlex, Inter, Font)
+  ( Font(Inter)
   , generate, generate'
-  , prettyHtml, wrap , wrapPost , Config(..)
-  , footer, generateHtml, navigation, navigationTemplate
-  , headTitle, partialHtml
+  , Config(..)
+  , footer
 
   -- For individual componenents
   , anchorBlue, anchorBlack
@@ -290,40 +290,6 @@ run Command.ListStories = listStories
 run Command.JsImportStories = jsImportStories
 
 run Command.JsStories = jsStories
-
-
---------------------------------------------------------------------------------
-generateTemplates :: Bool -> IO ()
-generateTemplates forGitHubPages = do
-  let config = if forGitHubPages
-               then
-                 -- Used to generate HTML for GitHub Pages
-                 Config "/static" IbmPlex False
-               else
-                 -- Used to generate the template.
-                 Config "$prefix$/static" (Font "$font$") False
-
-  -- TODO The $body$ is indented when using the pretty printer, which
-  -- then causes Pandoc to indent part of <code> content, which
-  -- is wrong. Same for lots of white space betwee navigation links, which
-  -- causes also some additional "padding".
-  generateHtml config "generated/templates" "default.html" "$title$"
-    (H.div (navigationTemplate >> wrapPost "$title$" "$body$") >> footer "$footer$")
-
-  -- TODO Currently reusing the default.html template.
-  prettyHtml config "generated/templates" "default-2-cols.html" "$title$"
-    (wrap "$body$" >> footer "$footer$")
-
-  -- TODO Currently reusing the default.html template.
-  prettyHtml config "generated/templates" "poster.html" "$title$"
-    (wrap "$body$" >> footer "$footer$")
-
-  -- We probably don't need the footer, navigation, and title partial
-  -- templates since they can be generated with the complete templates.
-
-  partialHtml config "generated/templates" "footer.html" "" (footer "$footer$")
-  partialHtml config "generated/templates" "navigation.html" "" (navigation ".")
-  partialHtml config "generated/templates" "title.html" "" headTitle
 
 
 ------------------------------------------------------------------------------
---
+++ Hypered/Design/Templates.hs
@@ -0,0 +1,46 @@
+module Hypered.Design.Templates
+  ( generateTemplates
+  ) where
+
+import           Hypered.Html
+  ( Font(IbmPlex, Font)
+  , prettyHtml, wrap , wrapPost , Config(..)
+  , footer, generateHtml, navigation, navigationTemplate
+  , headTitle, partialHtml
+  )
+import           Protolude
+import qualified Text.Blaze.Html5 as H
+
+
+--------------------------------------------------------------------------------
+generateTemplates :: Bool -> IO ()
+generateTemplates forGitHubPages = do
+  let config = if forGitHubPages
+               then
+                 -- Used to generate HTML for GitHub Pages
+                 Config "/static" IbmPlex False
+               else
+                 -- Used to generate the template.
+                 Config "$prefix$/static" (Font "$font$") False
+
+  -- TODO The $body$ is indented when using the pretty printer, which
+  -- then causes Pandoc to indent part of <code> content, which
+  -- is wrong. Same for lots of white space betwee navigation links, which
+  -- causes also some additional "padding".
+  generateHtml config "generated/templates" "default.html" "$title$"
+    (H.div (navigationTemplate >> wrapPost "$title$" "$body$") >> footer "$footer$")
+
+  -- TODO Currently reusing the default.html template.
+  prettyHtml config "generated/templates" "default-2-cols.html" "$title$"
+    (wrap "$body$" >> footer "$footer$")
+
+  -- TODO Currently reusing the default.html template.
+  prettyHtml config "generated/templates" "poster.html" "$title$"
+    (wrap "$body$" >> footer "$footer$")
+
+  -- We probably don't need the footer, navigation, and title partial
+  -- templates since they can be generated with the complete templates.
+
+  partialHtml config "generated/templates" "footer.html" "" (footer "$footer$")
+  partialHtml config "generated/templates" "navigation.html" "" (navigation ".")
+  partialHtml config "generated/templates" "title.html" "" headTitle
