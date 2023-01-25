86df1dbccf7b54e8f1b6236b6140e04cf58d2c1b
Vo Minh Thu <thu@hypered.io>
2023-01-25 20:16:48 +0100

    Add a derivation to build the PDF brochure.

---
+++ brochure/default.nix
@@ -0,0 +1,58 @@
+{ nixpkgs ? <nixpkgs>
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+in rec
+{
+  brochure = pkgs.stdenvNoCC.mkDerivation {
+    name = "brochure";
+    src = ./.;
+ 
+    buildInputs = [
+      pkgs.biber
+      (pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          anyfontsize
+          biblatex
+          biblatex-apa
+          booktabs
+          caption
+          changepage
+          datatool # provides datatool-base
+          enumitem
+          etoolbox
+          float
+          fontaxes
+          fontsize
+          glossaries
+          marginnote
+          mfirstuc
+          pagecolor
+          plex # provides e.g. plex-sans
+          scheme-basic
+          sidenotes
+          textcase
+          titlesec
+          xcolor
+          xfor
+          xkeyval
+          ;
+      })
+    ];
+ 
+    buildPhase = ''
+      pdflatex template
+      biber template
+      makeglossaries template
+      pdflatex template
+      pdflatex template
+    '';
+ 
+    installPhase = ''
+      mkdir -p $out
+      cp template.pdf $out/
+    '';
+  };
+}
