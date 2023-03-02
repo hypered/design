2d7f187970d4408d436284f4f2e19b0a9f7c1d69
Vo Minh Thu <thu@hypered.io>
2023-03-02 18:29:59 +0100

    Add some D3.js code in its own file.

--- d3/gulpfile.js
+++ d3/gulpfile.js
@@ -2,13 +2,14 @@ const D3Node = require('d3-node');
 const gulp = require('gulp');
 const file = require('gulp-file');
 
+const hypered = require('./hypered.js');
+
 
 function graphs() {
   const d3n = new D3Node();
-  d3n.createSVG(10,20).append('g');
-
+  hypered.drawOne(d3n);
   return file('out.svg', d3n.svgString(), {src: true})
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('dist/'));
 }
 
 exports.build = gulp.parallel(graphs);
