04b9294cda193ba8c0e16a0162ee165bc36b64ba
Vo Minh Thu <thu@hypered.io>
2024-03-07 12:03:31 +0100

    Move the ourOverrides function to contents.nix.
    This will make it easier to re-use it when we want to use
    horizon-platform (and in particular, the extend function to add our
    package to its packages, instead of using an overlay).

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,19 +4,10 @@ let
   lib = super.lib;
   sources = import ./sources.nix;
   contents = import ./contents.nix { nixpkgs = super; };
-  inherit (super.lib.attrsets) mapAttrs;
-  inherit (import sources.gitignore { inherit lib; }) gitignoreFilter;
-
-  ourOverrides = selfh: superh:
-    let
-      callCabalOn = name: dir:
-        selfh.callCabal2nix "${name}" dir { };
-
-    in mapAttrs callCabalOn contents.pkgList;
 
 in {
   haskellPackages = super.haskellPackages.override (old: {
     overrides =
-      lib.composeExtensions (old.overrides or (_: _: { })) ourOverrides;
+      lib.composeExtensions (old.overrides or (_: _: { })) contents.overrides;
   });
 }
