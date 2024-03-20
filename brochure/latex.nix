795dc99c6f3941fae4201edfb98da549e6cac050
Vo Minh Thu <thu@hypered.io>
2024-03-20 14:23:47 +0100

    Extract the LaTeX environment.
    This makes it possible to re-use it elsewhere.

---
+++ brochure/latex.nix
@@ -0,0 +1,32 @@
+let
+  sources = import ../nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in
+  pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      anyfontsize
+      biblatex
+      biblatex-apa
+      booktabs
+      caption
+      changepage
+      datatool # provides datatool-base
+      enumitem
+      etoolbox
+      float
+      fontaxes
+      fontsize
+      glossaries
+      marginnote
+      mfirstuc
+      pagecolor
+      plex # provides e.g. plex-sans
+      scheme-basic
+      sidenotes
+      textcase
+      titlesec
+      xcolor
+      xfor
+      xkeyval
+      ;
+  }
--- brochure/shell.nix
+++ brochure/shell.nix
@@ -6,33 +6,6 @@ in
     buildInputs = [
       pkgs.biber
       # pkgs.mupdf
-      (pkgs.texlive.combine {
-        inherit (pkgs.texlive)
-          anyfontsize
-          biblatex
-          biblatex-apa
-          booktabs
-          caption
-          changepage
-          datatool # provides datatool-base
-          enumitem
-          etoolbox
-          float
-          fontaxes
-          fontsize
-          glossaries
-          marginnote
-          mfirstuc
-          pagecolor
-          plex # provides e.g. plex-sans
-          scheme-basic
-          sidenotes
-          textcase
-          titlesec
-          xcolor
-          xfor
-          xkeyval
-          ;
-      })
+      (import ./latex.nix)
     ];
   } ""
